154b6a98bd3ea6abb988a6a539932c74d637077a
Nico Watine <nico@studioncreations.com>
2016-04-08 11:17:17 -0500

    Added new 'extra.js' file
    To represent additional script files not mass-processed via ‘npm run
    build’

---
+++ js/src/extra.js
@@ -0,0 +1,4 @@
+// This file is to show how 'app.js' is for importing all of the Foundation-specific files
+// 'extra.js' is a placehold file for any other script files
+// 'extra.js' is minified via codekit, not via 'npm run build'
+// when 'npm run build' is executed, 'extra-min.js' is simply copied over and not processed in any way
