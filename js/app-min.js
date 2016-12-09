b475e7e6f1d7d4e4358d007650b3053afbe48394
Nico Watine <nico@studioncreations.com>
2016-12-09 13:06:47 -0600

    Codekit now just COPIES app.js (doesn’t PROCESS)

--- js/app-min.js
+++ js/app-min.js
@@ -1,1 +1,5 @@
 $(document).foundation();
+
+// yo yo yo
+/////// and a bottle of rum for checking minification (word?)
+
--- js/src/app.js
+++ js/src/app.js
@@ -2,3 +2,4 @@ $(document).foundation();
 
 // yo yo yo
 /////// and a bottle of rum for checking minification (word?)
+
