05a2b50207d6d34f92b3c82e1c5fc58bde4b9590
Nico Watine <nico@studioncreations.com>
2017-12-15 10:20:08 -0600

    Add border around box-shadow:0 for visibility

--- src/pages/index.php
+++ src/pages/index.php
@@ -98,8 +98,8 @@ body-class:
 	<div class="large-9 columns">
 		<div class="row">
 			<div class="large-4 columns text-center">
-				<div class="boxShadow-1" style="height: 100px; background: #f9f9f9;">
-					<code>box-shadow: $z-1;</code>
+				<div style="border: 1px solid #e6e6e6; height: 100px; background: #f9f9f9;">
+					<code>box-shadow: 0;</code>
 				</div>
 				<br>
 			</div>
